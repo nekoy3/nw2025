1a72a4793fbd241c7a4d2f2f28cd4932a60d843e
nekoy3 <84169441+nekoy3@users.noreply.github.com>
2025-06-30 12:39:00 +0900

    HTTP/1.0でファイルを見られるようにした

--- src/httpserver1.rb
+++ src/httpserver1.rb
@@ -14,10 +14,43 @@ loop do
     # クライアントからのデータを受け取り
     while line=sock.gets
       print line
-      sock.print(line) #受け取った内容をクライアントに送り返す
+      if /\AGET/ =~ line then
+        path = line.split(' ')[1]
+        # case文を使った条件分岐
+        case path
+          when "/hello"
+            sock.puts("HTTP/1.0 200 OK")
+            sock.puts
+            sock.puts("Hello, World!!")
+          when "/301"
+            sock.puts("HTTP/1.0 301 Moved Permanently")
+            sock.puts("Location: http://www.kyusan-u.ac.jp")
+            sock.puts
+          else
+            #ファイルを表示する、なければ404を返す
+            #filename = path.slice(1..)
+            filename = "." + path #ドット突っ込んでカレントディレクトリにしてもいい（おもしろい）
+            if File.exist?(filename) then
+              sock.puts("HTTP/1.0 200 OK")
+              sock.puts "Content-Type: text/plain; charset=UTF-8"
+              sock.puts
+              file = File.open(filename, "r")
+              while line = file.gets&.chomp
+                sock.puts line
+              end
+            else
+              sock.puts("HTTP/1.0 404 Not Found")
+              sock.puts "Content-Type: text/plain; charset=UTF-8"
+              sock.puts
+              sock.puts("ファイルないよん ^ω^")
+            end
+        end
+        
+        break
+      end
 
       # 受信した行が空行だったらwhileを抜ける
-      break if line == "\r\n"
+      #break if line == "\r\n"
     end
     sock.close
     sleep 60
